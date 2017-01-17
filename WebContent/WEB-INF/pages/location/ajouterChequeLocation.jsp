c0049df53ea6d3048c70c69cf8242d0a34acc53d
fabdu <fabdu@MSI>
2017-01-17 10:12:10 +0100

    Ajout du formulaire ajouter cheque

--- WebContent/WEB-INF/pages/location/ajouterChequeLocation.jsp
+++ WebContent/WEB-INF/pages/location/ajouterChequeLocation.jsp
@@ -15,4 +15,42 @@
 <h1>Ajouter un chèque</h1>
 
 
+<form id="ajoutCheque" action="#" method="post">
+	<fieldset>
+		<legend>Informations du chèque</legend>
+		
+		<div>
+			<label for="datePaiement" class='required'>Date de paiement </label> <input type="text"
+				name="datePaiement" class="datepicker" value="${cheque.getDatePaiement()}" required="required" />
+		</div>
+		
+		<div>
+			<label for="montantCheque" class='required'>Montant du chèque </label> <input type="text"
+				name="montantCheque" value="${cheque.getMontantCheque()}" required="required" />
+		</div>
+		
+		<div>
+			<label for="numeroCheque" class='required'>Numéro du chèque </label> <input type="text"
+				name="numeroCheque" value="${cheque.getNumCheque()}" required="required" />
+		</div>
+		
+		<div>
+			<label for="dateEncaissement" class='required'>Date d'encaissement </label> <input type="text"
+				name="dateEncaissement" class="datepicker" value="${cheque.getDateEncaissement()}" required="required" />
+		</div>
+		
+		
+		
+	
+	</fieldset>
+	
+	<fieldset class='align-center no-border'>
+		<p class="oblig">* Champs obligatoires</p>
+		<a href="<c:url value="<%= Pattern.ACCUEIL %>"/>">Annuler</a>
+		<input type="submit" class="btn" value="Enregistrer" />
+	</fieldset>
+</form>
+
+
+
 <c:import url="/inc/footer.inc.jsp" />
--- src/fr/gemao/view/location/AjouterChequeLocationServlet.java
+++ src/fr/gemao/view/location/AjouterChequeLocationServlet.java
@@ -11,6 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import fr.gemao.ctrl.location.LocationCtrl;
+import fr.gemao.entity.materiel.ChequeLocation;
+import fr.gemao.entity.materiel.Location;
+import fr.gemao.entity.materiel.TypeLocation;
+import fr.gemao.form.location.LocationForm;
+import fr.gemao.form.util.Form;
 import fr.gemao.view.JSPFile;
 import fr.gemao.view.Pattern;
 
@@ -24,6 +30,31 @@ public class AjouterChequeLocationServlet extends HttpServlet {
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		int idLoc = Integer.parseInt(request.getParameter("id"));
+		List<Location> locations = LocationCtrl.getAll();
+		Location location = null;
+		ChequeLocation cheque = null;
+		for(Location l : locations){
+			if(l.getId()==idLoc){
+				location = l;
+			}
+		}
+		
+		request.setAttribute("location", location);
+		request.setAttribute("cheque", cheque);
+		
 		this.getServletContext().getRequestDispatcher(JSPFile.LOCATION_CHEQUE_AJOUTER).forward(request,  response);
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//ChequeForm chequeForm = new ChequeForm(request);
+		HttpSession session = request.getSession();
+		
+		if(Form.getValeurChamp(request, "numeroCheque")!=null){
+			
+		}
+	}
+	
 }
